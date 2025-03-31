9496ab36dbc70bf0460b063bf964430d5c824bef
Alexandre Seyffert <alexseyf66@gmail.com>
2025-03-31 20:33:49 -0300

    edit vercel to index.ts

--- index.ts
+++ index.ts
@@ -1,21 +1,17 @@
 import express from 'express'
 import routesAdmins from './routes/admins'
-import routesAdminsAlunos from './routes/admins'
-import AdminsResponsaveis from './routes/admins'
 import routesLogin from './routes/login'
 import routesRecuperaSenha from './routes/recuperaSenha'
 import routesValidaSenha from './routes/validaSenha'
 import cors from 'cors'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
 
 app.use("/admins", routesAdmins)
-app.use("/admins/alunos", routesAdminsAlunos)
-app.use("/admins/responsaveis", AdminsResponsaveis)
 app.use("/login", routesLogin)
 app.use("/recupera-senha", routesRecuperaSenha)
 app.use("/valida-senha", routesValidaSenha)
