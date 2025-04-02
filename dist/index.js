3b9e4dbbd1da5c6c182c8dfcaab5f3616a5c9eb2
Alexandre Seyffert <alexseyf66@gmail.com>
2025-04-02 09:25:00 -0300

    add Access-Control-Allow-Origin to cors

--- dist/index.js
+++ dist/index.js
@@ -19,9 +19,8 @@ app.use((req, res, next) => {
 app.use((0, cors_1.default)({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Authorization'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
+    exposedHeaders: ['Authorization', 'Access-Control-Allow-Origin'],
 }));
 app.use(express_1.default.json());
 app.use((req, res, next) => {
--- index.ts
+++ index.ts
@@ -17,9 +17,8 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  exposedHeaders: ['Authorization'], 
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'], 
+  exposedHeaders: ['Authorization', 'Access-Control-Allow-Origin'],
 }))
 
 app.use(express.json())
